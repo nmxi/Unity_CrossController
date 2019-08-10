2cd96dfdc268b4730bb5648c4e812dfff77c754f
nmxi <nagatsukimxi@gmail.com>
2019-08-10 22:45:57 +0900

    UI部分の完成，コントローラ用コンポーネント作成

---
+++ Assets/Scripts/CrossController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace dev.kemomimi.crossController
+{
+    public class CrossController : MonoBehaviour
+    {
+        public void OnStartDrag()
+        {
+            Debug.Log("Start drag");
+        }
+
+        public void OnEndDrag()
+        {
+            Debug.Log("End drag");
+        }
+    }
+}
